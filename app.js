3bc6b68b8b964d7c52b8b9bfbe92a14473561402
unknown <sejalshrivastav51@gmail.com>
2023-07-28 15:28:13 +0530

    update in static asset configuration

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
-app.use(express.static("public/"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
 const categories=[];
